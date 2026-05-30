9f8901a4f0092a9e8c7fb78338edc00bd2b55b70
Bartosz Machniak <b.machniak01@gmail.com>
2026-05-30 21:27:00 +0200

    Turn debug off

--- record/record/settings.py
+++ record/record/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-is_^m0$*=j-)!!&ujgjox2%2!jg0i9k&79c+k1*2r$mcy0ltq5"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "192.168.0.109",
