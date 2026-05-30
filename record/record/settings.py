53dc3f993f4f4f7d97c1fbd6592391e0a6169f00
Bartosz Machniak <b.machniak01@gmail.com>
2026-05-30 21:48:18 +0200

    style fix 2

--- record/record/settings.py
+++ record/record/settings.py
@@ -132,7 +132,14 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Media files
 MEDIA_URL = "/media/"
